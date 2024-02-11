9b975841f648a9a7ee5605fa46f0a5a89db6e070
“Khalid” <khalidali708882@gmail.com>
2024-02-11 19:50:45 +0530

    data ingestion completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,67 @@
+# Import all the required libraries
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.utils import read_sql_data
+
+# from src.components.data_transformation import DataTransformation, DataTransformationConfig
+# from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
+# Initialize Data Ingestion Configuration
+@dataclass
+class DataIngestionConfig:
+    raw_data_path: str = os.path.join('artifacts','data.csv')
+    train_data_path: str = os.path.join('artifacts','train.csv')
+    test_data_path: str = os.path.join('artifacts','test.csv')
+    
+# Create a class for Data Ingestion
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initate_data_ingestion(self):
+        logging.info('Data ingestion Started')
+        try:
+            # dataset reading from my sql database
+            # df = read_sql_data()
+
+            #read from file
+            df = pd.read_csv('notebook/data/diamonds.csv')
+            logging.info('Dataset read from my sql database')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            
+            logging.info('splitting data into train and test datasets')
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=0)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info('data_ingestion is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            logging.info('Exception occured at Data Ingestion stage')
+            raise CustomException(e, sys)
+    
+# Run Data ingestion
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data, test_data = obj.initate_data_ingestion()
+
+    # data_transformation = DataTransformation()
+    # train_arr, test_arr, _ = data_transformation.initate_data_transformation(train_data,test_data)
+
+    # modeltrainer = ModelTrainer()
+    # modeltrainer.initate_model_training(train_arr, test_arr)
--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,14 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
-
 import pymysql
 from dotenv import load_dotenv
 
+import dill
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+#data reading from sql database(.env file has been also created for basic database info)
+#used in data_ingestion
 load_dotenv()
 host=os.getenv("host")
 user=os.getenv("user")
@@ -28,6 +32,94 @@ def read_sql_data():
         df = pd.read_sql_query('Select * from diamonds',mydb)
         print(df.head())
         return df
-    except Exception as ex:
-        raise CustomException(ex,sys)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+# for saving directories
+# used in data transformation 
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(xtrain,ytrain,xtest,ytest,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(xtrain,ytrain)
+
+            # Predict Training data
+            y_train_pred = model.predict(xtrain)
+
+            # Predict Testing data
+            y_test_pred =model.predict(xtest)
+
+            # Get R2 scores for train and test data
+            train_model_score = r2_score(ytrain,y_train_pred)
+            test_model_score = r2_score(ytest,y_test_pred)
+
+            report[list(models.keys())[i]] =  test_model_score
+
+        return report
+
+    except Exception as e:
+        logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
+    
+def model_metrics(true, predicted):
+    try :
+        mae = mean_absolute_error(true, predicted)
+        mse = mean_squared_error(true, predicted)
+        rmse = np.sqrt(mse)
+        r2_square = r2_score(true, predicted)
+        return mae, rmse, r2_square
+    except Exception as e:
+        logging.info('Exception Occured while evaluating metric')
+        raise CustomException(e,sys)
+    
+
+def print_evaluated_results(xtrain,ytrain,xtest,ytest,model):
+    try:
+        ytrain_pred = model.predict(xtrain)
+        ytest_pred = model.predict(xtest)
+
+        # Evaluate Train and Test dataset
+        model_train_mae , model_train_rmse, model_train_r2 = model_metrics(ytrain, ytrain_pred)
+        model_test_mae , model_test_rmse, model_test_r2 = model_metrics(ytest, ytest_pred)
+
+        # Printing results
+        print('Model performance for Training set')
+        print("- Root Mean Squared Error: {:.4f}".format(model_train_rmse))
+        print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
+        print("- R2 Score: {:.4f}".format(model_train_r2))
+
+        print('----------------------------------')
+    
+        print('Model performance for Test set')
+        print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
+        print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
+        print("- R2 Score: {:.4f}".format(model_test_r2))
+    
+    except Exception as e:
+        logging.info('Exception occured during printing of evaluated results')
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
 
